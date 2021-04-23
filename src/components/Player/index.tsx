e9d571ad8d831fa907ec955f818466e2bb231f3e
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-04-22 23:43:04 -0300

    Dia 3 #nlw - estilização da home do podecastr

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -16,7 +16,7 @@ export function Player() {
         <footer className={styles.empty}>
             <div className={styles.progress}>
                 <span>00:00</span>
-                <div className-={styles.slider}>
+                <div className={styles.slider}>
                     <div className={styles.emptySlider} />
                 </div>
                 <span>00:00</span>
